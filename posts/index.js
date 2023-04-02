6f7b9b963e7a6e17379c89bae0a7678e1bbf5f56
Rigo Antonio Galicia Barrera <rigo.galicia@icloud.com>
2023-04-02 09:39:01 -0600

    The deploy and service of the event bus was configured

--- posts/index.js
+++ posts/index.js
@@ -22,7 +22,7 @@ app.post('/posts', async (req, res) => {
     posts[id] = { id, title };
 
     try {
-        await axios.post('http://localhost:4000/events', {
+        await axios.post('http://event-bus-srv:4000/events', {
             type: 'PostCreated',
             data: { id, title }
         });
